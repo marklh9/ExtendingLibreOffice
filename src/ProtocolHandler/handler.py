da222b594e6afa7ae8b13446ea6ea66f74cec82c
Mark Hung <marklh9@gmail.com>
2018-01-03 23:16:12 +0800

    Format code and convert to unix LF

--- src/ProtocolHandler/handler.py
+++ src/ProtocolHandler/handler.py
@@ -6,37 +6,24 @@ from com.sun.star.frame import XDispatchProvider
 from com.sun.star.lang import XInitialization
 
 
-#logging.basicConfig(filename='/tmp/protocl_handler.txt', level=logging.DEBUG)
+# logging.basicConfig(filename='/tmp/protocl_handler.txt', level=logging.DEBUG)
 
 def show_msg(title, msg, toolkit, frame):
-    from com.sun.star.awt.WindowClass import MODALTOP
-    from com.sun.star.awt.WindowAttribute import BORDER, MOVEABLE, CLOSEABLE
-    from com.sun.star.awt import WindowDescriptor
-    from com.sun.star.awt import Rectangle
-
-    descriptor = WindowDescriptor()
-    descriptor.Type = MODALTOP
-    descriptor.WindowServiceName = "infobox"
-    descriptor.ParentIndex = -1
-    descriptor.Parent = frame.getContainerWindow()
-    descriptor.Bounds = Rectangle(0, 0, 300, 200)
-    descriptor.WindowAttributes = BORDER | MOVEABLE | CLOSEABLE
-
-    msgbox = toolkit.createWindow(descriptor)
-    if msgbox is not None:
-        msgbox.CaptionText = title
-        msgbox.MessageText = msg
-        msgbox.execute()
+    from com.sun.star.awt.MessageBoxType import INFOBOX
+    from com.sun.star.awt.MessageBoxButtons import DEFAULT_BUTTON_OK
+    msgbox = toolkit.createMessageBox(frame.getContainerWindow(), INFOBOX, \
+                                      DEFAULT_BUTTON_OK, title, msg)
+    msgbox.execute()
 
 
 class SampleHandler(unohelper.Base, XDispatchProvider, XDispatch, XInitialization):
     def __init__(self, ctx):
         self.ctx = ctx
         self.frame = None
-        self.toolkit = ctx.getServiceManager().\
+        self.toolkit = ctx.getServiceManager(). \
             createInstanceWithContext("com.sun.star.awt.Toolkit", ctx)
 
-    def initialize(self,objs):
+    def initialize(self, objs):
         logging.debug("initialize called.")
         if len(objs) > 0:
             self.frame = objs[0]
@@ -51,17 +38,17 @@ class SampleHandler(unohelper.Base, XDispatchProvider, XDispatch, XInitializatio
             return
 
     def addStatusListener(self, listener, url):
-        logging.debug("addStatusListener:"+str(url))
+        logging.debug("addStatusListener:" + str(url))
         return
 
     def removeStatusListener(self, listener, url):
-        logging.debug("removeStatusListener:"+str(url))
+        logging.debug("removeStatusListener:" + str(url))
         return
 
     def queryDispatch(self, url, target, searchflags):
-        logging.debug("queryDispatch url:"+str(url))
+        logging.debug("queryDispatch url:" + str(url))
         if url.Protocol == "Dummy:" and url.Path == "do_something":
-                return self
+            return self
         return None
 
     def queryDispatches(self, requests):
