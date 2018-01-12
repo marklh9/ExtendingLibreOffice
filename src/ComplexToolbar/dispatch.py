15d92c379dd664507ec0074124519d65c99cfd25
Mark Hung <marklh9@gmail.com>
2018-01-12 13:08:53 +0800

    Refactor

--- src/ComplexToolbar/dispatch.py
+++ src/ComplexToolbar/dispatch.py
@@ -3,9 +3,39 @@ import unohelper
 import uno
 
 from com.sun.star.frame import XDispatch
-from feature_event import new_event
+from feature_event import FeatureEventWrapper
 
 #logging.basicConfig(filename='/tmp/complex_toolbar.txt', level=logging.DEBUG)
+
+class StatusListenerWrapper():
+    def __init__(self, listener, url):
+        self.listener = listener
+        self.url = url
+
+    def send_command(self, command, name, value):
+        event = FeatureEventWrapper( self.url, True, False )
+        self.listener.statusChanged( event.set_command( command, name, value ) )
+        return self
+
+    def send_command_with_args(self, command, args):
+        event = FeatureEventWrapper( self.url, True, False )
+        self.listener.statusChanged( event.set_command_with_args( command, args ) )
+        return self
+
+    def change_state(self, state):
+        event = FeatureEventWrapper( self.url, True, False )
+        self.listener.statusChanged( event.set_state( command, state) )
+        return self
+
+def init_list(x):
+    # Force typing to prevent it from becoming sequence of Any objects.
+    thelist = uno.Any( "[]string", ("Apple","Banana","Orange") )
+    x.send_command( "SetList", "List", thelist )
+    x.send_command( "AddEntry", "Text", "She" )
+    x.send_command( "AddEntry", "Text", "Sell" )
+    x.send_command( "AddEntry", "Text", "Seashell" )
+    x.send_command( "AddEntry", "Text", "Seashore" )
+    x.send_command( "SetDropDownLines", "Lines", 3 )
 
 class SampleDispatch(unohelper.Base, XDispatch):
     def __init__(self, ctx, args):
@@ -29,29 +59,21 @@ class SampleDispatch(unohelper.Base, XDispatch):
             return
 
 
-    def init_list(self, listener, url):
-        # Force typing to prevent it from becoming sequence of Any objects.
-        thelist = uno.Any( "[]string", ("Apple","Banana","Orange") )
-        listener.statusChanged( new_event(self, url).set_list( thelist ) )
-
-        listener.statusChanged( new_event(self, url).add_entry( "She" ) )
-        listener.statusChanged( new_event(self, url).add_entry( "Sell" ) )
-        listener.statusChanged( new_event(self, url).add_entry( "Seashell" ) )
-        listener.statusChanged( new_event(self, url).add_entry( "Seashore" ) )
-
-        listener.statusChanged( new_event(self, url).set_dropdown_lines( 3 ) )
-
     def init_text(self, listener, url,text):
-        listener.statusChanged( new_event(self, url).set_text(text) )
+        x = StatusListenerWrapper( listener, url )
+        x.send_command( "SetText", "Text", text )
 
     def set_values(self, listener, url, value, step, lower, upper):
-        listener.statusChanged( new_event(self, url).set_values(value, step, lower, upper) )
+        args = { "Value" : value, "Step" : step, "LowerLimit": lower, "UpperLimit" : upper }
+        x = StatusListenerWrapper( listener, url )
+        x.send_command_with_args( "SetValues", args )
 
     def set_image(self, listener, url, image_url):
-        listener.statusChanged( new_event(self, url).set_image(image_url) )
+        x = StatusListenerWrapper( listener, url )
 
     def addStatusListener(self, listener, url):
         try:
+            x = StatusListenerWrapper( listener, url )
             #show_msg("dispatch", "Hello, there!", self.toolkit, self.frame)
             logging.debug("addStatusListener Path="+url.Path)
             if url.Path == "ImageButton":
@@ -59,21 +81,21 @@ class SampleDispatch(unohelper.Base, XDispatch):
                 self.set_image(listener, url, image_url )
                 self.image_button = listener
             if url.Path == "Combobox":
-                self.init_list(listener,url)
+                init_list(x)
                 self.init_text(listener, url, "Dummy text for combobox")
             if url.Path == "Dropdownbox":
-                self.init_list(listener,url)
+                init_list(x)
                 self.list_item = listener
             if url.Path == "Spinfield":
                 self.set_values(listener, url, 10, 2, 0, 20)
             if url.Path == "Editfield":
                 self.init_text(listener, url, "Dummy text for editfield")
             if url.Path == "Dropdownbox":
-                self.init_list(listener,url)
+                init_list(x)
             if url.Path == "DropdownButton":
-                self.init_list(listener,url)
+                init_list(x)
             if url.Path == "ToggleDropdownButton":
-                self.init_list(listener,url)
+                init_list(x)
 
         except:
             logging.exception("dispatch")
--- src/ComplexToolbar/pythonpath/feature_event.py
+++ src/ComplexToolbar/pythonpath/feature_event.py
@@ -16,57 +16,26 @@ def CreateControlCommand( command, arguments ):
     return control_command
 
 class FeatureEventWrapper():
-    def __init__(self, source, url, enabled, requery ):
+    def __init__(self, url, enabled, requery ):
         event = FeatureStateEvent()
-        event.Source = source
         event.FeatureURL = url
         event.IsEnabled  = enabled
         event.Requery    = requery
         self.event = event
-        # The listener acts based on type of the event.State
-        # Any(bool) -> make visible and checked / unchecked
-        # Any(string) -> SetItemText / SetQuickHelpText
-        # Any(ItemStatus)
-        # Any(ItemVisibility) -> toggle visibility
-        # Any(ControlCommand) -> execute control command
-
-
-    def get_event(self):
-        return self.event
 
     def set_command(self, command, argname, argvalue):
         args  = (CreateNamedValue(argname, argvalue), )
         self.event.State = CreateControlCommand( command, args )
         return self.event
 
-    def set_image(self, image_url):
-        return self.set_command( "SetImage", "URL", image_url )
-
-    def set_text(self, text):
-        return self.set_command( "SetText", "Text", text )
-
-    def set_list(self, thelist):
-        return self.set_command( "SetList", "List", thelist )
-
-    def add_entry(self, text ):
-        return self.set_command( "AddEntry", "Text", text )
-
-    def set_dropdown_lines(self, lines ):
-        return self.set_command( "SetDropDownLines", "Lines", lines )
-
-    def set_values(self, value, step, lower, upper):
-        a1 = CreateNamedValue( "Value", value )
-        a2 = CreateNamedValue( "Step", step )
-        a3 = CreateNamedValue( "UpperLimit", upper )
-        a4 = CreateNamedValue( "LowerLimit", lower )
-        args = ( a1, a2, a3, a4, )
-        self.event.State = CreateControlCommand( "SetValues", args )
+    def set_command_with_args(self, command, args):
+        arglist = []
+        for name, value in args.items():
+            arglist.append( CreateNamedValue( name, value ) )
+        self.event.State = CreateControlCommand( command, tuple(arglist) )
         return self.event
 
     def set_state(self, state ):
         self.event.State = state
         return self.event
-
-def new_event(listener,url):
-    return FeatureEventWrapper(listener, url, True, False)
 
